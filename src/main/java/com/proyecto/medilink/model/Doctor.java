515dcbc747ff1640a3fe8aeb1f39a7afbe7b14d3
ronaldo isacc <u22208906@utp.edu.pe>
2026-06-29 11:42:54 -0500

    feat:solucionado de duplicidad

--- src/main/java/com/proyecto/medilink/model/Doctor.java
+++ src/main/java/com/proyecto/medilink/model/Doctor.java
@@ -1,7 +1,6 @@
 package com.proyecto.medilink.model;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.*;
 import lombok.Data;
 
 @Entity
@@ -13,24 +12,17 @@ public class Doctor {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotBlank(message = "El nombre es obligatorio")
     private String nombre;
 
-    @NotBlank(message = "Los apellidos son obligatorios")
     private String apellidos;
 
-    @NotBlank(message = "La especialidad es obligatoria")
     private String especialidad;
 
-    @NotBlank(message = "El número de colegiatura es obligatorio")
     private String numeroColegiatura;
 
-    @Email(message = "Formato de email incorrecto")
-    @NotBlank(message = "El email es obligatorio")
-    @Column(unique = true)
+    @Column(unique = true, nullable = false)
     private String email;
 
-    @Pattern(regexp = "^[0-9]{9}$", message = "El teléfono debe tener 9 dígitos")
     private String telefono;
 
     // Nombre de archivo de la imagen del doctor almacenada en /static/img/doctors/
