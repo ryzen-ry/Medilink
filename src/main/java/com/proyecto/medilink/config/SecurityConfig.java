8eeeeb08da1ccf969430e5bde81b6c8610018eb3
ronaldo isacc <u22208906@utp.edu.pe>
2026-06-29 14:54:42 -0500

    feat:aplicando buenas practicas

--- src/main/java/com/proyecto/medilink/config/SecurityConfig.java
+++ src/main/java/com/proyecto/medilink/config/SecurityConfig.java
@@ -5,10 +5,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.*;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
