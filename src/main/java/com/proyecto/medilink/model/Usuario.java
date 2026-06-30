45b3c932dc3180b4b419a27d57fbac32f878b119
ronaldo isacc <u22208906@utp.edu.pe>
2026-06-29 20:56:49 -0500

    sesionado

--- src/main/java/com/proyecto/medilink/model/Usuario.java
+++ src/main/java/com/proyecto/medilink/model/Usuario.java
@@ -2,11 +2,14 @@ package com.proyecto.medilink.model;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import java.io.Serializable;
 
 @Entity
 @Data
 @Table(name = "usuarios")
-public class Usuario {
+public class Usuario implements Serializable {
+
+    private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
